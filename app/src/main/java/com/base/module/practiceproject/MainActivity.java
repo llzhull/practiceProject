8c1343892c77493518c3e051993a42670185a8dc
llzhull <15658809326@163.com>
2023-07-26 14:46:09 +0800

    third commit

--- app/src/main/java/com/base/module/practiceproject/MainActivity.java
+++ app/src/main/java/com/base/module/practiceproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.base.module.practiceproject;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,10 +14,14 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
         super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND, WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+        Log.e(TAG, "onCreate: " );
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -25,7 +32,32 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        
+
     }
 
+//    @Override
+//    public void onAttachedToWindow() {
+//        Log.e(TAG, "onAttachedToWindow: ");
+//        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
+//        super.onAttachedToWindow();
+//
+//    }
+
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        Log.e(TAG, "onKeyDown: keyCode = " + keyCode + ",keyevent = " + event);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onKeyUp(int keyCode, KeyEvent event) {
+//        Log.e(TAG, "onKeyUp: keyCode = " + keyCode + ",keyevent = " + event);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean dispatchKeyEvent(KeyEvent event) {
+//        Log.e(TAG, "dispatchKeyEvent: keyevent = " + event);
+//        return true;
+//    }
 }
